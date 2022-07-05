34d1e8407a274192e83373de8c2a71247e5243a6
ZenonStella <stellazenon@gmail.com>
2022-07-05 16:22:21 +0200

    cookies

--- controller/settings-controller.php
+++ controller/settings-controller.php
@@ -1,5 +1,5 @@
 <?php
-var_dump($_SESSION);
+// var_dump($_SESSION);
 var_dump($_POST);
 // redirection si l'utilisateur n'est pas loggé
 if (!isset($_SESSION['USER'])) {
@@ -8,19 +8,24 @@ if (!isset($_SESSION['USER'])) {
 }
 
 // var_dump de contrôle
-// var_dump($_POST);
 
 // Nous recherchons si il y a une validation de formulaire via une méthode POST (Ca correspond à une validation via un bouton)
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // setcookie("mySearch", $_POST['mySearch'], time() + 3600);
-    // setcookie("myView", $_POST['myView'], time() + 3600);
+    // var_dump($_POST['myCheckbox']);
+    for ($i = 0; $i <= 2; $i++) {
+        if (isset($_POST['myCheckbox'][$i])) {
+            setcookie("myCheckbox$i", $_POST['myCheckbox'][$i], time() + 3600);
+            // echo 'ok';
+        }
+    }
+    setcookie("myView", $_POST['myView'], time() + 3600);
 
     // nous controllons si la checkbox valentineTheme est coché
-    if (isset($_POST['valentineTheme'])) {
-        setcookie("valentineTheme", $_POST['valentineTheme'], time() + 3600);
-    } else {
-        setcookie("valentineTheme", '', time() - 3600);
-    }
+    // if (isset($_POST['valentineTheme'])) {
+    //     setcookie("valentineTheme", $_POST['valentineTheme'], time() + 3600);
+    // } else {
+    //     setcookie("valentineTheme", '', time() - 3600);
+    // }
 
     // refresh de la page à l'aide d'un header location
     // header('Location: ../views/settings.php');
