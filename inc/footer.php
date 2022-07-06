d2e3e8b930976b4a10ed3d2268a415e56df348a4
Eosiin <jordan.mabille76@outlook.fr>
2022-07-06 12:47:56 +0200

    footer ok

--- inc/footer.php
+++ inc/footer.php
@@ -1,6 +1,6 @@
 </div>
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top  bg-dark">
-    <div class="col-md-4 d-flex align-items-center">
+<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mb-0 bg-dark">
+    <div class="col-md-4 d-flex align-items-center ">
         <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1 text-light">
             <svg class="bi" width="30" height="24">
                 <use xlink:href="#bootstrap" />
