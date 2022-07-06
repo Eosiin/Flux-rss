e1f732d6edecdfec498da3fac20e62bebc5be8ef
ZenonStella <stellazenon@gmail.com>
2022-07-06 16:53:44 +0200

    footer mobil

--- inc/footer.php
+++ inc/footer.php
@@ -1,15 +1,9 @@
 </div>
-<footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top mb-0 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "bg-light"  : "bg-dark" ?>">
-
+<footer class="row flex-wrap justify-content-between align-items-center py-2 my-3 border-top mb-0 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "bg-light"  : "bg-dark" ?>">
     <div class="col-md-4 d-flex align-items-center ">
-        <a href="/" class="mb-3 me-2 mb-md-0 text-decoration-none lh-1 text-light">
-            <svg class="bi" width="30" height="24">
-                <use xlink:href="#bootstrap" />
-            </svg>
-        </a>
         <span class="mb-3 mb-md-0 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "text-dark" : "text-light" ?>">&copy; 2022 Company, Inc</span>
     </div>
-    <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <ul class="nav col-md-4 me-2 justify-content-end list-unstyled d-flex">
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-twitch.png" alt=""></a></li>
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-yt.png" alt=""></a></li>
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-discord.png" alt=""></a></li>
