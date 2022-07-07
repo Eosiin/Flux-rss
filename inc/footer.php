c73c9239642b7765efc59e8985f8fa9e9b725972
ZenonStella <stellazenon@gmail.com>
2022-07-07 09:18:56 +0200

    footer mentions légales

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,10 @@
 </div>
-<footer class="row flex-wrap justify-content-between align-items-center py-2 my-3 border-top mb-0 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "bg-light"  : "bg-dark" ?>">
-    <div class="col-md-4 d-flex align-items-center ">
+<footer class="row flex-wrap justify-content-between align-items-center py-2 px-5 my-3 mb-2 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "bg-light"  : "bg-dark" ?>">
+    <div class="col-md-4 d-flex align-items-center justify-content-center">
         <span class="mb-3 mb-md-0 <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "text-dark" : "text-light" ?>">&copy; 2022 Company, Inc</span>
     </div>
     <ul class="nav col-md-4 me-2 justify-content-end list-unstyled d-flex">
+        <li class=" <?= (isset($_SESSION['USER']) && isset($_COOKIE["whiteTheme"]) && $_COOKIE["whiteTheme"] == "on") ? "text-dark" : "text-light" ?>">Mentions Légales</li>
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-twitch.png" alt=""></a></li>
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-yt.png" alt=""></a></li>
         <li class="ms-3 logo-reseaux"><a class="text-light" href="#"><img class="reseaux" src="../assets/img/logo-discord.png" alt=""></a></li>
